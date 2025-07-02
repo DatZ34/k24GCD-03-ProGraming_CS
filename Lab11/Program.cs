6bde874d856855e16ad2ad53683eabcd5e33e1d8
DatZ34 <vothanhdat4265@gmail.com>
2025-07-02 11:43:26 +0700

    OnTap

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -19,7 +19,7 @@ namespace Lab11
         {
             Dictionary<int, Player> playerList = new Dictionary<int, Player>();
             await Bai1();
-
+            await Bai2();
         }
         public static async Task vd2()
         {
@@ -36,7 +36,7 @@ namespace Lab11
                 allPlayers = JsonConvert.DeserializeObject<List<Player>>(json);
                 var goldPlayers = allPlayers
                     .ToList();
-                int index = 0;
+                int index = 1;
                 foreach (Player player in goldPlayers)
                 {
 
@@ -69,7 +69,7 @@ namespace Lab11
                     .Where(p => p.Gold > 1000 && p.Coins > 100)
                     
                     .ToList();
-                int index = 0;
+                int index = 1;
                
                 foreach (var p in goldPlayers)
                 {
@@ -78,6 +78,56 @@ namespace Lab11
                     await firebase.Child("quiz_bai1_richPlayers").Child(index.ToString()).PutAsync(p);
                     index++;
                   
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static async Task Bai2()
+        {
+            var firebase = new FirebaseClient(firebase_URl);
+            string url = "https://raw.githubusercontent.com/NTH-VTC/OnlineDemoC-/main/simple_players.json";
+
+            HttpClient client = new HttpClient();
+            string serverConfig = await client.GetStringAsync(url);
+            List<Player> allPlayers;
+            try
+            {
+                string json = await client.GetStringAsync("https://raw.githubusercontent.com/NTH-VTC/OnlineDemoC-/main/simple_players.json");
+
+                allPlayers = JsonConvert.DeserializeObject<List<Player>>(json);
+                var VipPlayerRegion = allPlayers
+                    .Where(p => p.VipLevel > 0)
+                    .GroupBy(p => p.Region)
+                    
+                    .Select(p => new
+                    {
+                        Vip = p.Key,
+                        Count = p.Count()
+                    })
+                    
+                   
+                    .ToList();
+                foreach(var vip in VipPlayerRegion)
+                {
+                    Console.WriteLine($"khu Vực: {vip.Vip}, số người chơi VIP : {vip.Count}");
+                }
+                DateTime now = new DateTime(2025, 06, 30, 0, 0, 0);
+                var LoginPlayer = allPlayers
+                    .Where(p => (now - p.LastLogin).TotalDays <= 2)
+                    .ToList();
+                //Console.WriteLine($"Tổng số người chơi có Viplevel > 0 là : {CountVipPlayer}");
+                int index = 1;
+                foreach (var p in LoginPlayer)
+                {
+                    Console.WriteLine($"Name: {p.Name}, Viplevel: {p.VipLevel}, LastLogin: {p.LastLogin}");
+
+                    await firebase.Child("quiz_bai2_recentVipPlayers").Child(index.ToString()).PutAsync(p);
+                    index++;
+
 
                 }
             }
